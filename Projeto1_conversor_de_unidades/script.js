4fb32d81ac7e03ac9b31ad4375015bc730630cc2
Bruno Alejandro <brunoalejandro367@gmail.com>
2023-09-30 21:09:14 -0300

    adição do conversor de capacidade

--- Projeto1_conversor_de_unidades/script.js
+++ Projeto1_conversor_de_unidades/script.js
@@ -1,12 +1,18 @@
 // Selecionar os elementos
 let inputElement = document.querySelector('#input')
+let inputElement2 = document.querySelector('#input2')
 let outputElement = document.querySelector('#output')
+let outputElement2 = document.querySelector('#output2')
 let fromElement = document.querySelector('#from')
 let toElement = document.querySelector('#to')
+let fromElement2 = document.querySelector('#from2')
+let toElement2 = document.querySelector('#to2')
 let convertBtn = document.querySelector('#convert-btn')
+let convertBtn2 = document.querySelector('#convert-btn2')
 let messageElement = document.querySelector('#message')
+let messageElement2 = document.querySelector('#message2')
 
-function convertValues(){
+function convertValuesComp(){
     const fromValue = fromElement.value;
     const toValue = toElement.value
     const inputValue = Number(inputElement.value)
@@ -209,4 +215,210 @@ function convertValues(){
 
     return
 }
-convertBtn.addEventListener('click', convertValues)
+convertBtn.addEventListener('click', convertValuesComp)
+
+/* FUNÇÃO 2 - VOLUME */
+function convertValuesVol(){
+    const fromValue2 = fromElement2.value;
+    const toValue2 = toElement2.value
+    const inputValue2 = Number(inputElement2.value)
+
+    // validação para o caso de unidades iguais
+    if(fromValue2 === toValue2){
+        outputElement2.value = inputElement2.value
+        messageElement2.textContent = 'As unidades escolhidas são iguais.'
+        return
+    }
+
+    let result2 
+    switch (fromValue2) {
+        case 'kl':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2
+                    break
+                case 'hl':
+                    result2 = inputValue2 * 10
+                    break;
+                case 'dal':
+                    result2 = inputValue2 * 100
+                    break;
+                case 'l':
+                    result2 = inputValue2 * 1000
+                    break;
+                case 'dl':
+                    result2 = inputValue2 * 10000
+                    break;
+                case 'cl':
+                    result2 = inputValue2 * 100000
+                    break;
+                case 'ml':
+                    result2 = inputValue2 * 1000000
+                    break;
+            }
+        break;
+        case 'hl':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2 / 10
+                    break;
+                case 'hl':
+                    result2 = inputValue2 
+                    break;
+                case 'dal':
+                    result2 = inputValue2 * 10
+                    break;
+                case 'l':
+                    result2 = inputValue2 * 100
+                    break;
+                case 'dl':
+                    result2 = inputValue2 * 1000
+                    break;
+                case 'cl':
+                    result2 = inputValue2 * 10000
+                    break
+                case 'ml':
+                    result2 = inputValue2 * 100000
+                    break
+            }
+        break;
+        case 'dl':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2 / 100
+                    break;
+                case 'hl':
+                    result2 = inputValue2 / 10 
+                    break;
+                case 'dal':
+                    result2 = inputValue2 
+                    break;
+                case 'l':
+                    result2 = inputValue2 * 10
+                    break;
+                case 'dl':
+                    result2 = inputValue2 * 100
+                    break;
+                case 'cl':
+                    result2 = inputValue2 * 1000
+                    break;
+                case 'ml':
+                    result2 = inputValue2 * 10000
+                    break;
+            }
+        break;
+        case 'l':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2 / 1000
+                    break;
+                case 'hl':
+                    result2 = inputValue2 / 100
+                    break;
+                case 'dal':
+                    result2 = inputValue2 / 10
+                    break;
+                case 'l':
+                    result2 = inputValue2
+                    break;
+                case 'dl':
+                    result2 = inputValue2 * 10
+                    break;
+                case 'cl':
+                    result2 = inputValue2 * 100
+                    break;
+                case 'ml':
+                    result2 = inputValue2 * 1000
+                    break;
+            }
+        break;
+        case 'dl':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2 / 10000
+                    break;
+                case 'hl':
+                    result2 = inputValue2 / 1000
+                    break;
+                case 'dal':
+                    result2 = inputValue2 / 100
+                    break;
+                case 'l':
+                    result2 = inputValue2 / 10
+                    break;
+                case 'dl':
+                    result2 = inputValue2
+                    break;
+                case 'cl':
+                    result2 = inputValue2 * 10
+                    break;
+                case 'ml':
+                    result2 = inputValue2 * 100
+                    break;
+            }
+        break;
+        case 'cl':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2 / 100000
+                    break;
+                case 'hl':
+                    result2 = inputValue2 / 10000
+                    break;
+                case 'dal':
+                    result2 = inputValue2 / 1000
+                    break;
+                case 'l':
+                    result2 = inputValue2 / 100
+                    break;
+                case 'dl':
+                    result2 = inputValue2 / 10
+                    break;
+                case 'cl':
+                    result2 = inputValue2
+                    break;
+                case 'ml':
+                    result2 = inputValue2 * 10
+                    break;
+            }
+        break;
+        case 'ml':
+            switch (toValue2) {
+                case 'kl':
+                    result2 = inputValue2 / 1000000
+                    break;
+                case 'hl':
+                    result2 = inputValue2 / 100000
+                    break;
+                case 'dal':
+                    result2 = inputValue2 / 10000
+                    break;
+                case 'l':
+                    result2 = inputValue2 / 1000
+                    break;
+                case 'dl':
+                    result2 = inputValue2 / 100
+                    break;
+                case 'cl':
+                    result2 = inputValue2 / 10
+                    break;
+                case 'ml':
+                    result2 = inputValue2
+                    break;
+            }
+        break;
+    }
+    // Exibir resultado no input
+    messageElement2.textContent = '';
+    outputElement2.value = result2
+
+    // Exibir resultado na mensagem
+    const fromLabel2 = fromElement2.options[fromElement2.selectedIndex].text
+    const toLabel2 = toElement2.options[toElement2.selectedIndex].text
+    
+    const message2 = `${inputElement2.value} ${fromLabel2} equivalem a ${result2} ${toLabel2}`
+    messageElement2.textContent = message2
+
+    return
+}
+convertBtn2.addEventListener('click', convertValuesVol)
